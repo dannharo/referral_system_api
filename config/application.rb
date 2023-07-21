c372d8a5fb042a57a56524a6261f388bab011bf1
Luis Payan <luis.payan@bigcommerce.com>
2023-07-21 11:04:38 -0600

    RS-0 Add encryption keys

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,9 @@ module ReferralSystemApi
     config.middleware.use ActionDispatch::Session::CookieStore
 
     config.autoload_paths << "#{Rails.root}/lib"
+
+    config.active_record.encryption.primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
   end
 end
