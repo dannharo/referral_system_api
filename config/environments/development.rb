7d28a23d871b36e3c558f8917606eaa69f127160
Carlos <carlos.lopez@bigcommerce.com>
2023-11-02 17:30:25 -0600

    Adding known host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.hosts << "referral-system-api.onrender.com"
+  config.hosts << "referral-system-api-pro.azurewebsites.net"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
